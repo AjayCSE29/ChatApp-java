cbf77726f796cfbe5b396ac2cffd8570bdcd379f
AjayCSE29 <110125104016@aalimec.ac.in>
2026-09-05 21:01:20 +0530

    Added User-name appearance, chat visibility and append upcoming names and Chats

--- Main.java
+++ Main.java
@@ -4,11 +4,39 @@ import java.io.*;
 import java.awt.Color;
 import java.awt.Font;
 
-
-
 public class Main{
+	
+  public static void AddUserCard(String name, JPanel panel){
+		JPanel usrcrd = new JPanel(new GridBagLayout());
+		JLabel usrname = new JLabel(name);
+		usrname.setForeground(Color.WHITE);
+		usrcrd.setBackground(Color.BLACK);
+		usrcrd.setMaximumSize(new Dimension(180, 40));
+		usrname.setFont(new Font("Arial", Font.BOLD, 15));
+		usrcrd.add(usrname);
+		panel.add(usrcrd);
+		
+		panel.revalidate();
+		panel.repaint();
+		
+  }
+  
+  public static void sendMessage(String usrmessage, JPanel panel){
+	  JPanel usrmsg = new JPanel(new GridBagLayout());
+	  JLabel message = new JLabel(usrmessage);
+	  usrmsg.setBackground(Color.BLACK);
+	  message.setForeground(Color.WHITE);
+	  usrmsg.setMaximumSize(new Dimension(120, 40));
+	  message.setFont(new Font("Arial", Font.BOLD, 12));
+	  usrmsg.add(message);
+	  panel.add(usrmsg);
+	  
+	  panel.revalidate();
+	  panel.repaint();
+	  
+  }
 
-  public static void ChatInterface(){
+  public static void ChatInterface(String name){
   
     JFrame frame = new JFrame("Chat Window");
     frame.setLayout(new BorderLayout());
@@ -22,7 +50,7 @@ public class Main{
     Rightpanel.setBackground(Color.WHITE);
 	JPanel BottomPanel = new JPanel(new BorderLayout());
 	
-	JPanel title = new JPanel(new BorderLayout());
+	JPanel title = new JPanel(new GridBagLayout());
 	title.setBackground(Color.BLUE);
 	title.setPreferredSize(new Dimension(180, 40));
 	JLabel t1 = new JLabel("Users");
@@ -44,11 +72,27 @@ public class Main{
 	send.setForeground(Color.WHITE);
 	BottomPanel.add(send, BorderLayout.EAST);
 	
-	Leftpanel.add(title, BorderLayout.NORTH);
-	title.add(t1, BorderLayout.CENTER);
+	JPanel leftContainer = new JPanel();
+	leftContainer.setLayout(new BoxLayout(leftContainer, BoxLayout.Y_AXIS));
 
+	JPanel rightContainer = new JPanel();
+	rightContainer.setLayout(new BoxLayout(rightContainer, BoxLayout.Y_AXIS));
+
+	Leftpanel.add(leftContainer, BorderLayout.CENTER);
+	Rightpanel.add(rightContainer, BorderLayout.CENTER);
+	
+	Leftpanel.add(title, BorderLayout.NORTH);
+	title.add(t1);
+	
+	AddUserCard(name, leftContainer);
 	
     frame.setVisible(true);
+	
+	send.addActionListener(e -> {
+		String message = chat.getText();
+		sendMessage(message, rightContainer);
+	});
+	
   }
 
   public static void main(String[] args){
@@ -95,8 +139,8 @@ public class Main{
     b1.addActionListener(e -> {
       String name = new String(tf1.getText());
       JOptionPane.showMessageDialog(frame, "Logged in as " + name);
-      frame.setVisible(false);
-      ChatInterface();
+      frame.dispose();
+      ChatInterface(name);
     });
     
     frame.add(panel);
